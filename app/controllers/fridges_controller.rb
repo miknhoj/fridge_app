8935213bff74bceba8fc854e7af544722d038606
miknhoj <kimjohan86@gmail.com>
2018-10-23 15:10:58 -0400

    show route added to fridge

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -4,11 +4,13 @@ class FridgesController < ApplicationController
     @fridges = Fridge.all
   end
 
-
   def new
+    @fridge = Fridge.new
   end
 
   def show
+    fridge_id = params[:id]
+    @fridge = Fridge.find_by(id: fridge_id)
   end
 
   def create
